36b0908aec9bb7316b44dfb434a608b14253119c
Sang-Geun Ahn <tkdrms0301@naver.com>
2023-11-06 02:34:30 +0900

    error modify

--- OhBANK-main/BackendServer/lib/api/Beneficiary/approve.js
+++ OhBANK-main/BackendServer/lib/api/Beneficiary/approve.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Model = require('../../../models/index');
-var Response = require('../../Response');
-var statusCodes = require('../../statusCodes');
+var Model = require("../../../models/index");
+var Response = require("../../Response");
+var statusCodes = require("../../statusCodes");
 var { validateAdminToken } = require("../../../middlewares/validateToken");
 var { encryptResponse, decryptRequest } = require("../../../middlewares/crypt");
 
@@ -14,61 +14,68 @@ var { encryptResponse, decryptRequest } = require("../../../middlewares/crypt");
  * @param id                         - ID to be approved
  * @return                           - Status
  */
-router.post('/', [validateAdminToken, decryptRequest], (req, res) => {
-    var r = new Response();
-    let { id } = req.body;
-
-    Model.beneficiaries.findOne({
-        where: {
-            id: id,
-            approved: false
-        }
-    }).then((data) => {
-        if (data) {
-            Model.users.findOne({
-                where: {
-                    account_number: data.beneficiary_account_number
-                }
-            }).then((data) => {
-                if (data) {
-                    Model.beneficiaries.update({
-                        approved: true
-                    }, {
-                        where: {
-                            id: id
-                        }
-                    }).then(() => {
-                        r.status = statusCodes.SUCCESS;
-                        r.data = {
-                            "message": "Sucess"
-                        }
-                        return res.json(encryptResponse(r));
-                    });
-                } else {
-                    r.status = statusCodes.BAD_INPUT;
-                    r.data = {
-                        "message": "Beneficiary with given account number doesn't exist"
-                    };
-                    return res.json(encryptResponse(r));
-                }
-            });
-
-        } else {
-            r.status = statusCodes.BAD_INPUT;
-            r.data = {
-                "message": "Pending beneficiary with given details not found"
+router.post("/", [validateAdminToken, decryptRequest], (req, res) => {
+  var r = new Response();
+  let { id } = req.body;
+  Model.beneficiaries
+    .findOne({
+      where: {
+        id: id,
+        approved: false,
+      },
+    })
+    .then((data) => {
+      if (data) {
+        Model.users
+          .findOne({
+            where: {
+              account_number: data.dataValues.beneficiary_account_number,
+            },
+          })
+          .then((data) => {
+            if (data) {
+              Model.beneficiaries
+                .update(
+                  {
+                    approved: true,
+                  },
+                  {
+                    where: {
+                      id: id,
+                    },
+                  }
+                )
+                .then(() => {
+                  r.status = statusCodes.SUCCESS;
+                  r.data = {
+                    message: "Sucess",
+                  };
+                  return res.json(encryptResponse(r));
+                });
+            } else {
+              r.status = statusCodes.BAD_INPUT;
+              r.data = {
+                message: "Beneficiary with given account number doesn't exist",
+              };
+              return res.json(encryptResponse(r));
             }
-            return res.json(encryptResponse(r));
-        }
-    }).catch((err) => {
-        console.log()
-        r.status = statusCodes.SERVER_ERROR;
+          });
+      } else {
+        r.status = statusCodes.BAD_INPUT;
         r.data = {
-            "message": err.toString()
+          message: "Pending beneficiary with given details not found",
         };
-        res.json(encryptResponse(r));
+        return res.json(encryptResponse(r));
+      }
+    })
+    .catch((err) => {
+      console.log();
+      r.status = statusCodes.SERVER_ERROR;
+      r.data = {
+        message: err.toString(),
+      };
+      res.json(encryptResponse(r));
     });
 });
-
 
 module.exports = router;
