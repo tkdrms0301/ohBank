73cbffaab4378f1c3a17d3c7ffba630937ff6d98
Sang-Geun Ahn <tkdrms0301@naver.com>
2023-11-14 05:33:29 +0900

    파일 업로드 수정_3

--- OhBANK-main/BackendServer/lib/api/Qna/fileup.js
+++ OhBANK-main/BackendServer/lib/api/Qna/fileup.js
@@ -24,28 +24,17 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     // Create safe file names
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const santiziedFilename = uniqueSuffix + '-' + file.originalname.replace(/[^a-zA-Z0-9]/g, '_');
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const santiziedFilename =
+      uniqueSuffix + "-" + file.originalname.replace(/[^a-zA-Z0-9]/g, "_");
     cb(null, santiziedFilename);
   },
 });
 
-const upload = multer({ 
+const upload = multer({
   storage: storage,
   limits: {
     fileSize: 1024 * 1024 * 5, // Limit file size to 5MB
-  },
-  fileFilter: (req, file, cb) => {
-    //Check file extension
-    const allowedExtensions = ['jpg','jpeg','png','pdf']; // List of allowed file extension
-
-    const fileNameParts = file.originalname.split('.');
-    const fileExtension = fileNameParts[fileNameParts.length - 1].toLowerCase();
-
-    if (!allowedExtensions.includes(fileExtension)) {
-      return cb(new Error('file extension not allowed.'));
-    }
-    cb(null, true);
   },
 });
 
@@ -54,22 +43,38 @@ router.post("/", upload.single("file"), validateUserToken, (req, res) => {
   let user_id = req.user_id;
   var filename = req.file.originalname;
   var savedname = req.file.destination + "/" + filename;
-  
-  Model.file.create({
-    file_name: filename,
-    saved_name: savedname,
-    user_id: user_id,
-  }).then((data) => {
-    r.status = statusCodes.SUCCESS;
-    r.data = data;
-    return res.json(encryptResponse(r));
-  }).catch((err) => {
-    r.status = statusCodes.SERVER_ERROR;
+
+  const allowedExtensions = ["jpg", "jpeg", "png", "pdf"]; // List of allowed file extension
+
+  const fileNameParts = req.file.originalname.split(".");
+  const fileExtension = fileNameParts[fileNameParts.length - 1].toLowerCase();
+
+  if (!allowedExtensions.includes(fileExtension)) {
+    r.status = statusCodes.BAD_INPUT;
     r.data = {
-      message: 'An error occurred while uploading the file.',
+      message: "invalid file extension",
     };
     return res.json(encryptResponse(r));
-  });
+  }
+
+  Model.file
+    .create({
+      file_name: filename,
+      saved_name: savedname,
+      user_id: user_id,
+    })
+    .then((data) => {
+      r.status = statusCodes.SUCCESS;
+      r.data = data;
+      return res.json(encryptResponse(r));
+    })
+    .catch((err) => {
+      r.status = statusCodes.SERVER_ERROR;
+      r.data = {
+        message: "server error",
+      };
+      return res.json(encryptResponse(r));
+    });
 });
 
 module.exports = router;
