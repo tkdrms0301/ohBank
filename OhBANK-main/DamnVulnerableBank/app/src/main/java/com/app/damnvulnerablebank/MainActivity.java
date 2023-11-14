1c88d174e78819916d2b7fdc19cb8e88d7a2646b
Sang-Geun Ahn <tkdrms0301@naver.com>
2023-11-15 03:49:21 +0900

    KeyStore RSA 암복호화_2

--- OhBANK-main/DamnVulnerableBank/app/src/main/java/com/app/damnvulnerablebank/MainActivity.java
+++ OhBANK-main/DamnVulnerableBank/app/src/main/java/com/app/damnvulnerablebank/MainActivity.java
@@ -73,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Check frida
-
         if(fridaCheck.fridaCheck() == 1) {
             Toast.makeText(getApplicationContext(), "Frida is running", Toast.LENGTH_SHORT).show();
             Log.d("FRIDA CHECK", "FRIDA Server DETECTED");
@@ -84,10 +83,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Frida is NOT running", Toast.LENGTH_SHORT).show();
         }
 
-
-
-
-
         SharedPreferences sharedPreferences = getSharedPreferences("jwt", Context.MODE_PRIVATE);
         boolean isloggedin=sharedPreferences.getBoolean("isloggedin", false);
         if(isloggedin)
@@ -97,8 +92,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
-
 
     public void loginPage(View view){
         Intent intent =new Intent(getApplicationContext(), BankLogin.class);
@@ -111,6 +104,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void healthCheck(View v){
+        SharedPreferences secretPref = getSharedPreferences("secret", MODE_PRIVATE);
+        SharedPreferences.Editor secretEditor = secretPref.edit();
+//        keystore encryption
+//        key : bf3c199c2470cb477d907b1e0917c17b
+//        iv : 5183666c72eec9e4
+        secretEditor.putString("key", "JO1QhyBz6ywDQ8sm5vWJCk3z55vFmQ9DWQaXRI1P2s56jsKpILo1CM7y5/74+TwQ/m12sIUjbpfOQAhjHjcWoYxitR7Djq5jyHgFJS6VuGiT2elhqmQnQOhLEmPAr1R4Y+nJHyjCjdLDAwqKFtA7WJiXBJCOirrKra69fO+4R4C9MeQW6P2IQ5wBDRfwcl8VkKnlZyQzbSUcKQSUAUPYZUD92f+5YbTRgGsiSH8rnx7p1PJXkeMeWUqsA9GQC8K5UrWh1tMlsblH2oG+xqvWrDaZUb4E1xrNJf8sGOJM+jsGlFXOxgZLDSa2dvqwUDdmZDhNd1T7hGo4Ttk4U/gbXg==");
+        secretEditor.putString("iv", "BZZz4cueSgUnd3cpLgbxmQ+3BEzQiaE9SkklQBw1ZNesWaKNLYxInuC4drzSsG3QktZdyva5PnzwwYItG//E7xUwMVPsoYEDBW1O9/ytd5lOgFp6KoXzieCq9tlgIatck0cBZAZ8Mc+qBmtDRVoCpQ4YVAeqDVCHPcwhYoLRqSyy2//tZ/iCeOQFmCCM5/Ka6ni/dcJ6WUs6y/Gq5avPwaeInhno3ne6JhygO2C5lTwweEVe/oKt5KM/IOLsbvfDIxeevZHuSs9q+VIgM+kKQr9XdCJr9CBXaFweYobg6U+Z+vQTtQcgGiH3TF7i8PKCuBKzwnixn+KpxleJmTd6aQ==");
+        secretEditor.apply();
+        EncryptDecrypt.getInstance().setKey(secretPref.getString("key", null));
+        EncryptDecrypt.getInstance().setIv(secretPref.getString("iv", null));
+
+        Log.d("ENCRYPTED", EncryptDecrypt.getInstance().getKey());
+        Log.d("ENCRYPTED", EncryptDecrypt.getInstance().getIv());
+
         SharedPreferences pref = getApplicationContext().getSharedPreferences("apiurl", MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         EditText ed=findViewById(R.id.apiurl);
